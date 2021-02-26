1bec4eddf81b2bcff989ddc1a52d58ea491197e2
sloanlacey <sloanlacey89@gmail.com>
2021-02-25 20:36:50 -0800

    Update books.js in models file with eslint guide.

--- models/book.js
+++ models/book.js
@@ -2,23 +2,23 @@
 const orm = require('../config/orm.js');
 
 const book = {
-    selectAll: function(cb) {
-        orm.selectAll('books', (res) => {
-            cb(res);
-        })
-    },
+  selectAll: function(cb) {
+    orm.selectAll('books', (res) => {
+      cb(res);
+    });
+  },
 
-    insertOne: function(cols, vals, cb) {
-        orm.insertOne('books', cols, vals, (res) => {
-            cb(res);
-        })
-    },
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne('books', cols, vals, (res) => {
+      cb(res);
+    });
+  },
 
-    updateOne: function(objColVals, condition, cb) {
-        orm.updateOne('books', objColVals, condition, (res) => {
-            cb(res);
-        })
-    }
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne('books', objColVals, condition, (res) => {
+      cb(res);
+    });
+  }
 };
 // Export burger for use by model
 module.exports = book;
