d1503582ddda6e373a0b269257e09b307de1221a
sloanlacey <sloanlacey89@gmail.com>
2021-02-25 20:22:03 -0800

    Update book_controller.js with eslint guide.

--- controllers/book_controller.js
+++ controllers/book_controller.js
@@ -8,51 +8,52 @@ const googleKey = 'AIzaSyDT1yyStTIyISfsmZ0T0AnShc606GKUPAk';
 const router = express.Router();
 // Home route
 router.get('/', (req, res) => {
-    book.selectAll(function(data) {
-        const hbsObject = {
-            books: data
-        };
-        res.render('index', hbsObject);
-    })
+  book.selectAll(function(data) {
+    const hbsObject = {
+      books: data
+    };
+    res.render('index', hbsObject);
+  });
 });
 
 router.get('/search/:term', (req, res) => {
-    axios.get(`https://www.googleapis.com/books/v1/volumes?q=${req.params.term}&key=${googleKey}&maxResults=10`)
+  axios.get(`https://www.googleapis.com/books/v1/volumes?q=${req.params.term}&key=${googleKey}&maxResults=10`)
     .then(googleResponse => {
-            res.json(googleResponse.data);
-    }) 
+      res.json(googleResponse.data);
+    })
     .catch(console.error);
 });
 
 router.get('/book/:id', (req, res) => {
-    axios.get(`https://www.googleapis.com/books/v1/volumes/${req.params.id}?key=${googleKey}`)
+  axios.get(`https://www.googleapis.com/books/v1/volumes/${req.params.id}?key=${googleKey}`)
     .then(googleResponse => {
-            res.json(googleResponse.data);
-    }) 
+      res.json(googleResponse.data);
+    })
     .catch(console.error);
 });
 
 router.post('/api/books', (req, res) => {
-    book.insertOne(['book_name', 'have_read'], [req.body.book_name, req.body.have_read], (result) => {
-        res.json({ id: result.insertId })
-    })
+  book.insertOne(['book_name', 'have_read'], [req.body.book_name, req.body.have_read], (result) => {
+    res.json({ id: result.insertId });
+  });
 });
 // Route for individual burgers by their id
 router.put('/api/books/:id', (req, res) => {
-    const condition = `id = ${req.params.id}`;
+  const condition = `id = ${req.params.id}`;
 
-    book.updateOne(
-        {
-            have_read: req.body.have_read
-        },
-        condition,
-        (result) => {
-            if(result.changedRows === 0) {
-                return res.status(404).end();
-            }
-            res.status(200).end();
-        }
-    )
+  book.updateOne(
+    {
+      // eslint-disable-next-line camelcase
+      have_read: req.body.have_read
+    },
+    condition,
+    (result) => {
+      if(result.changedRows === 0) {
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
 });
 
 module.exports = router;
