c070435cd12a661ed2f927714fd956aa77265877
Destiny Fleming <destiny.fleming7401@gmail.com>
2021-02-27 10:30:34 -0800

    added sequilze and config folder

--- controllers/book_controller.js
+++ controllers/book_controller.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 const book = require('../models/book.js');
 const googleKey = 'AIzaSyDT1yyStTIyISfsmZ0T0AnShc606GKUPAk';
 
+
 // Create router function
 const router = express.Router();
 // Home route
@@ -58,32 +59,38 @@ router.put('/api/books/:id', (req, res) => {
 
 // open library
 //author
-router.get('/a/:author', (req, res) => {
-    
-    axios.get(`http://openlibrary.org/search.json?author=${req.params.author}`)
+/*router.get('/api/:author', (req, res) => { 
+  axios.get(`http://openlibrary.org/search.json?author=${req.params.author}`)
     .then(openLib => {
-            res.json(openLib.data);
+      res.json(openLib.data);
     }) 
     .catch(console.error);
 });
 
 //title
-router.get('/a/:title', (req, res) => {
-    
-    axios.get(`http://openlibrary.org/search.json?title=${req.params.title}`)
+router.get('/api/:title', (req, res) => {
+  axios.get(`http://openlibrary.org/search.json?title=${req.params.title}`)
     .then(openLib => {
-            res.json(openLib.data);
+      res.json(openLib.data);
     }) 
     .catch(console.error);
 });
 
 //subject
-router.get('/a/:subject', (req, res) => {
-    axios.get(`https://openlibrary.org/subjects/${req.params.subject}`)
+router.get('/api/:subject', (req, res) => {
+  axios.get(`https://openlibrary.org/subjects/${req.params.subject}`)
     .then(openLib => {
-            res.json(openLib.data);
-    }) 
+      res.json(openLib.data);
+    })
     .catch(console.error);
-});
+});*/
 
+//data
+router.get('/a/:subject', (req, res) => {
+    axios.get(`https://image-charts.com/chart?chs=700x190&chd=t:60,40&cht=p3&chl=Hello%7CWorld&chan&chf=ps0-0,lg,45,ffeb3b,0.2,f44336,1|ps0-1,lg,45,8bc34a,0.2,009688,1`)
+      .then(openLib => {
+        res.json(openLib.data);
+      })
+      .catch(console.error);
+  });
 module.exports = router;
