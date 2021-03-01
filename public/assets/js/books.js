41ad7c6c7e2be6fc440e2b47a5dd8423db5f2aa2
Michael Alexander <michael.m.alexander@outlook.com>
2021-02-28 20:19:12 -0800

    More functionality added to the Google API, some non-working code for Open Library API, starter code for progress bar

--- public/assets/js/books.js
+++ public/assets/js/books.js
@@ -1,4 +1,5 @@
 $(function() {
+  $("#reading-tracker progress").val(Math.random()*100); //finished books/total books
   $('.change-status').on('click', function() {
     const id = $(this).data('id');
 
@@ -28,10 +29,57 @@ $(function() {
         console.log(booksData);
         let html = '<ol>';
         for (let book of booksData.items) {
-          html += `<li>${book.volumeInfo.title}</li>`;
+          if(!book.volumeInfo.title || !book.volumeInfo.authors || !book.volumeInfo.description) {
+            continue; 
+          }
+          const descriptionArray = book.volumeInfo.description.split(". ");
+          const description = descriptionArray[0] + (descriptionArray.length > 1? " ...": ".");
+          html += "<li>";
+          if(book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.smallThumbnail){
+            html += `<img src="${book.volumeInfo.imageLinks.smallThumbnail}">`
+          }
+          html += `<p>Title: ${book.volumeInfo.title} <br> Author: ${book.volumeInfo.authors} <br> Description: ${description}</p>`;
+          html += `<button data-bookid="${book.id}">More Info</button></li>`
         }
         html += '</ol>';
         $('#search-results').html(html).show();
+        $('[data-bookid]').on("click", getBookId) 
       });
   });
+
+  function getBookId(){
+    const id = $(this).attr("data-bookid");
+    fetch(`/api/book/${id}`).then(res => res.json()).then(moreBookData);
+  }
+  function moreBookData(book) {
+    const moreInfo = book.volumeInfo.infoLink;
+    window.location.href =`${moreInfo}`; 
+  }
 });
+
+$('.openLibraryForm').on('submit', function(event) {
+  event.preventDefault();
+  const term = $('#readBook').val();
+  fetch(`/api/title/${term}`)
+    .then(res => res.json())
+    .then(booksData => {
+      console.log(booksData);
+      let html = '<ol>';
+      for (let book of booksData.docs) {
+        if(!book.docs.title || !book.docs.author_name || !book.docs.subject || !book.docs.has_fulltext) {
+          continue; 
+        }
+        html += `<p>Title: ${book.docs.title} <br> Author: ${book.docs.author_name} <br> Subject: ${book.docs.subject}</p>`;
+        html += `<button data-readbook="${book.docs.has_fulltext}">Read Now!</button></li>`
+      }
+      html += '</ol>';
+      $('#search-results-2').html(html).show();
+      $('[data-readbook]').on("click", readBook) 
+    });
+});
+
+function readBook(book) {
+  const readThisBook = book.docs.isbn;
+  window.location.href =`https://openlibrary.org/isbn/${readThisBook}`; 
+}
+
