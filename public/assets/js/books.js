4627a20486ddd7dffdb77bb1fabc514da6b6a895
Michael Alexander <michael.m.alexander@outlook.com>
2021-03-02 23:03:47 -0800

    Random modal working, some changes to the tracker

--- public/assets/js/books.js
+++ public/assets/js/books.js
@@ -107,9 +107,9 @@ $(function () {
           return $('#random-button').click();
         }
         html = `${html}`;
-        $('#shelvesModal .modal-body').html(html);
+        $('.random-body').html(html);
         // eslint-disable-next-line no-use-before-define
-        $('#shelvesModal [data-isbn]').on('click', addToBookshelf);
+        $('#random-save').on('click', addToBookshelf);
         // $('#random-book').html(html).show();
         // eslint-disable-next-line no-use-before-define
       });
@@ -128,7 +128,7 @@ $(function () {
     const value = $('#value').val();
     const value2 = $('#value2').val();
     const value3 = $('#value3').val();
-    const chartPercent = `&chd=t:${value},${value2},${value3}`
+    const chartPercent = `&chd=t:${value},${value2}`
     const chartSize ='&chs=700x190';
     //const chartTxt = '&chl=Hi|From|data|api';
     const word = $('#word').text();
