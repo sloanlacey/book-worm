271e5e5a3fcd5c7371d6674ba522d1deb086ddcb
Michael Alexander <michael.m.alexander@outlook.com>
2021-03-04 19:45:12 -0800

    working bookshelf code

--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,22 @@ DROP DATABASE IF EXISTS books_db;
 
 CREATE DATABASE books_db;
 
-USE books_db;
+-- USE books_db;
 
-CREATE TABLE books (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    book_name VARCHAR(200),
-    have_read BOOLEAN
-);
+-- CREATE TABLE book (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     userid INT,
+--     percentread INT,
+--     book_name VARCHAR(200),
+--     have_read BOOLEAN,
+--     apiID VARCHAR(100),
+--     FOREIGN KEY (userid) references user(id)
+-- );
+
+-- CREATE TABLE user (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     email VARCHAR(50),
+--     password VARCHAR (50),
+--     createdAt VARCHAR(50),
+--     updatedAT VARCHAR(50)
+-- );
